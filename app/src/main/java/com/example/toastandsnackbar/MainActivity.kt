f75e4eeb96a430b864615d647fab7d87de442bc6
ReihanFatilla <reihanfatilla@gmail.com>
2022-07-18 14:00:15 +0700

    [feat]: Finished Custom Toast using MotionToast

--- app/src/main/java/com/example/toastandsnackbar/MainActivity.kt
+++ app/src/main/java/com/example/toastandsnackbar/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.toastandsnackbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import com.example.toastandsnackbar.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
+import www.sanju.motiontoast.MotionToast
+import www.sanju.motiontoast.MotionToastStyle
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,13 +18,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnSnackbar.setOnClickListener {
-            Snackbar.make(binding.root, "SnackBar Showed!", Snackbar.LENGTH_LONG)
-                .show()
+
+//            Snackbar.make(binding.root, "SnackBar Showed!", Snackbar.LENGTH_SHORT)
+//                .setAction("Retry"){
+//                    Snackbar.make(binding.root, "Retry Clicked!", Snackbar.LENGTH_SHORT)
+//                        .show()
+//                }
+//                .show()
         }
 
         binding.btnToast.setOnClickListener {
-            Toast.makeText(this, "Toast Showed!", Toast.LENGTH_SHORT)
-                .show()
+            MotionToast.createColorToast(this,"Toast Showed!",
+                "Succesfully showing a custom toast",
+                MotionToastStyle.SUCCESS,
+                MotionToast.GRAVITY_BOTTOM,
+                MotionToast.LONG_DURATION,
+                ResourcesCompat.getFont(this,R.font.helvetica_regular))
         }
     }
 }
